43b90037031c2bec590c749798b1cf73a9c5a138
weterbogov <nekete155@gmail.com>
2017-09-26 12:20:52 +0300

    Write elementary functions

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,45 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (typeof config == 'undefined') {
+            throw Error;
+        }
+        this.config = config;
+        this.activeState = config.initial;
+        this.stateStack = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.activeState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        this.stateStack.push(activeState);
+        this.activeState = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) { }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.stateStack = [];
+        this.activeState = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +49,44 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (typeof event == 'undefined') {
+            return this.config.states;
+        }
+        var returnStates = [];
+        for (var state in this.config.states) {
+            for (var tempEvent in state.transitions) {
+                if (event == tempEvent) {
+                    returnStates.push(state);
+                }
+            }
+        }
+        return returnStates;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        var previousState = this.stateStack.pop();
+        return (typeof previousState == 'undefined') ? false : previousState;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() { }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.stateStack = [];
+    }
 }
 
 module.exports = FSM;
